e0b30a8f87b6197e8324605c9fed2cee64249cab
MoureshEzhil <141090065+Moureshezhilarasu@users.noreply.github.com>
2025-12-12 11:49:55 +0530

    chore: add VS Code Java launch configuration

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Hello , WELCOME!!!!!!");
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        System.out.println(str);
     }
 }
